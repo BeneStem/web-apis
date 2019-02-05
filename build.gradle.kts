217cca469d3faea0e7ede5c185515fecd9fcdfdf
Benedikt Stemmildt <benedikt@stemmildt.com>
2019-02-05 08:59:35 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.run.BootRun
+
+apply {
+  plugin("idea")
+  plugin("kotlin")
+  plugin("kotlin-spring")
+  plugin("org.springframework.boot")
+  plugin("io.spring.dependency-management")
+  plugin("com.github.ben-manes.versions")
+
+  from("$rootDir/gradle/libraries.gradle.kts")
+}
+val libraries = extra["libraries"] as Map<*, *>
+
+buildscript {
+  repositories {
+    maven { setUrl("https://plugins.gradle.org/m2/") }
+  }
+  project.apply {
+    from("$rootDir/gradle/gradlePlugins.gradle.kts")
+  }
+  val gradlePlugins = extra["gradlePlugins"] as Map<*, *>
+  dependencies {
+    gradlePlugins.mapValues { classpath(it.value as String) }
+  }
+}
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+  jcenter()
+}
+
+dependencies {
+  "compile"(libraries["kotlin-stdlib-jre8"] as String)
+  "compile"(libraries["kotlin-reflect"] as String)
+  "compile"(libraries["jackson-module-kotlin"] as String)
+
+  "compile"(libraries["spring-boot-starter-webflux"] as String)
+
+  "compile"(libraries["spring-boot-devtools"] as String)
+  "compileOnly"(libraries["spring-context-indexer"] as String)
+}
+
+tasks {
+  withType<KotlinCompile> {
+    kotlinOptions {
+      freeCompilerArgs = listOf("-Xjsr305=strict")
+      jvmTarget = org.gradle.api.JavaVersion.VERSION_1_8.toString()
+    }
+  }
+
+  withType<BootRun> {
+    systemProperties = System.getProperties().mapKeys { it.key.toString() }.toMap()
+  }
+}
