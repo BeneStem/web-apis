9661baaf25b8f9ceb6eb88724f8cc991422202ff
Benedikt Stemmildt <benedikt@stemmildt.com>
2020-03-05 11:41:34 +0100

    lecture 3

--- src/main/kotlin/de/dhbw/webapis/client/repository/ProductRepository.kt
+++ src/main/kotlin/de/dhbw/webapis/client/repository/ProductRepository.kt
@@ -1,7 +1,8 @@
 package de.dhbw.webapis.client.repository
 
 import de.dhbw.webapis.common.domain.Product
-import org.springframework.http.MediaType.TEXT_EVENT_STREAM
+import de.dhbw.webapis.common.domain.Product.Companion.APPLICATION_PRODUCT_JSON_VALUE
+import org.springframework.http.MediaType.parseMediaType
 import org.springframework.stereotype.Repository
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
@@ -11,7 +12,20 @@ import reactor.core.publisher.Flux
 @Repository
 class ProductRepository {
 
+    private val webClient = WebClient.create("http://localhost:8081")
+
     fun find(search: String?, cheaperThan: Int?): Flux<Product> {
-       TODO()
+        val uri = UriComponentsBuilder.fromPath("/products")
+        if (search != null) {
+            uri.queryParam("search", search)
+        }
+        if (cheaperThan != null) {
+            uri.queryParam("cheaperThan", cheaperThan)
+        }
+        return webClient.get()
+                .uri(uri.toUriString())
+                .accept(parseMediaType(APPLICATION_PRODUCT_JSON_VALUE))
+                .retrieve()
+                .bodyToFlux()
     }
 }
